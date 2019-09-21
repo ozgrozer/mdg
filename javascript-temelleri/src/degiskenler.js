deda88fa8ecf443f18f31e9015efe2617b18d0b9
Ozgur Ozer <ozgr@live.com>
2019-09-21 15:21:14 +0300

    JavaScript temelleri değişkenler.

---
+++ javascript-temelleri/src/degiskenler.js
@@ -0,0 +1,12 @@
+// es5
+var test1 = 'deneme1'
+test1 = 'deneme2'
+console.log(test1)
+
+// es6
+const test2 = 'deneme2'
+console.log(test2)
+
+let test3 = 'deneme3'
+test3 = 'deneme4'
+console.log(test3)
